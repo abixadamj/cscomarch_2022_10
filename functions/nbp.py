10388ccdec6de635d26b48f12c47fcc657a26ee8
rulewicz <wojciech.paulewicz@gmail.com>
2022-10-19 15:59:24 +0200

    drobne zmiany

--- functions/nbp.py
+++ functions/nbp.py
@@ -1,1 +1,12 @@
 # Wojtek
+
+class Nbp_table:
+    def __init__(self, nowy_kolor):
+        self.nazwa = "Jakaś tam nazwa"
+        self.kolor = nowy_kolor
+
+    def jaki_kolor(self):
+        print(f"Narzędzie ma kolor {self.kolor} i nazywa się {self.nazwa}")
+
+srb_czerw = Nbp_table("czerwony")
+srb_czerw.jaki_kolor()
