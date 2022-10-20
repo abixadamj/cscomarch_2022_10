9c5f7c4cfac2552abc9efc41e7e4886060d86648
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2022-10-20 12:54:00 +0200

    md_create_init

--- day_3/markdown_creation.py
+++ day_3/markdown_creation.py
@@ -1,5 +1,3 @@
-import snakemd
-
 from functions.nbp import Nbp_table
 import snakemd
 
@@ -8,6 +6,19 @@ class MarkdownDoc:
     def __init__(self, filename: str, code: str, table: str = "A"):
         self.file: str = filename
         self.code: str = code
+        self.table: str = table
         self.mid: float = 0
         self.doc = snakemd.new_doc(self.file)
+        self.nbp = Nbp_table(self.table, self.code)
+
+        if self.nbp.get_table():
+            self.mid = self.nbp.kurs
+            self.doc.add_header("Kurs Waluty z dnia.")
+            self.doc.add_horizontal_rule()
+            # ...
+            self.doc.output_page()
+
+
+if __name__ == "__main__":
+    test_md = MarkdownDoc("pliczek", "CHF")
 
