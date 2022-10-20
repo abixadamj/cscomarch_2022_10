7ba474eda13223593372f5ad65b84ba3f7138cb7
ManekeenPIX <manekeenpix@gmail.com>
2022-10-20 11:45:38 +0200

    OK

---
+++ Pawel_Jankowski/Logs.py
@@ -0,0 +1,32 @@
+import socket
+from datetime import datetime
+
+
+
+class LogEvents:
+    """
+    FileLog
+    """
+    def __init__(self, logname: str):
+        self.logname:str = logname
+        self.filename:str = ""
+
+    def logMsg(self, sev: int, msg: str):
+        #with open(self.fileName, "a") as file_to_save:
+        hostname = socket.gethostname()
+        #print(hostname)
+        self.fileName = f"{hostname}_{self.logname}.txt"
+        #print(self.fileName)
+        try:
+            file_to_save = open(self.fileName, "a")
+        except:
+            print(f"Problem z otwarciem pliku {self.fileName}")
+            return False
+        timeNow = datetime.now()
+        file_to_save.write(f"{timeNow}\tsev: {sev}\tmsg: {msg}\n")
+
+testEvent = LogEvents("EventLog")
+testError = LogEvents("ErrorLog")
+
+testEvent.logMsg(1, "EVENT: Testowa wiadomosc")
+testError.logMsg(3, "ERROR: Testowa wiadomosc")
